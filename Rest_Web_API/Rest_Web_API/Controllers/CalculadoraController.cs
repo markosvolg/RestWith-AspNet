220c870797a79805092e564146ccf063f5147893
Marcos Antonio Santos da Silva <markosvolg@gmail.com>
2020-07-14 19:50:32 -0300

    implementando calculadora em Rest

---
+++ Rest_Web_API/Rest_Web_API/Controllers/CalculadoraController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rest_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        // GET api/values
+       
+        
+
+        // GET api/values/5
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public ActionResult Sum(string firstNumber , string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Valores Incorretos");
+        }
+
+
+        [HttpGet("substraction/{firstNumber}/{secondNumber}")]
+        public ActionResult Substraction(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Valores Incorretos");
+        }
+
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string strnumber)
+        {
+
+            double number;
+
+            bool isNumber = double.TryParse(strnumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+
+            return isNumber;
+        }
+    }
+}
