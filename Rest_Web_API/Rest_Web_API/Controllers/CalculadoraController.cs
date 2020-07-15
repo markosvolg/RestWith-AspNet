bcd0869c9185f3227834fa8781519515b1fad20d
Marcos Antonio Santos da Silva <markosvolg@gmail.com>
2020-07-15 17:14:20 -0300

    Criando a classe de Pessoas

--- Rest_Web_API/Rest_Web_API/Controllers/CalculadoraController.cs
+++ Rest_Web_API/Rest_Web_API/Controllers/CalculadoraController.cs
@@ -30,7 +30,7 @@ namespace Rest_Web_API.Controllers
         }
 
 
-        [HttpGet("substraction/{firstNumber}/{secondNumber}")]
+        [HttpGet("subtracao/{firstNumber}/{secondNumber}")]
         public ActionResult Substraction(string firstNumber, string secondNumber)
         {
 
@@ -43,6 +43,71 @@ namespace Rest_Web_API.Controllers
 
             return BadRequest("Valores Incorretos");
         }
+
+
+        [HttpGet("multiplicacao/{firstNumber}/{secondNumber}")]
+        public ActionResult Mutiplicacao(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var subtracao = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+
+                return Ok(subtracao.ToString());
+            }
+
+            return BadRequest("Valores Incorretos");
+
+        }
+
+
+
+        [HttpGet("divisao/{firstNumber}/{secondNumber}")]
+        public ActionResult Divisao(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var divisao = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+
+                return Ok(divisao.ToString());
+            }
+
+            return BadRequest("Valores Incorretos");
+        }
+
+
+        [HttpGet("Media/{firstNumber}/{secondNumber}")]
+        public ActionResult Media(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var media = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber))/2;
+
+                return Ok(media.ToString());
+            }
+
+            return BadRequest("Valores Incorretos");
+        }
+
+
+        [HttpGet("square-root/{number}")]
+        public ActionResult SquareRoot(string number)
+        {
+
+            if (IsNumeric(number))
+            {
+                var squareRoot = Math.Sqrt ((double)ConvertToDecimal(number));
+
+                return Ok(squareRoot.ToString());
+            }
+
+            return BadRequest("Valores Incorretos");
+        }
+
+
+
 
 
         private decimal ConvertToDecimal(string number)
