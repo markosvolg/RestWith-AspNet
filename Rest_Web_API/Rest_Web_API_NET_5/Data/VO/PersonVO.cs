d51d9278772cc8945ed97ff1b3e2bda77e2b2146
Marcos Antonio Santos da Silva <markosvolg@gmail.com>
2021-02-27 13:41:17 -0300

    Serialization JsonPropetyName

--- Rest_Web_API/Rest_Web_API_NET_5/Data/VO/PersonVO.cs
+++ Rest_Web_API/Rest_Web_API_NET_5/Data/VO/PersonVO.cs
@@ -1,15 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Rest_Web_API_NET_5.Data.VO
 {
     [Table("persons")]
     public class PersonVO
     {
-
+        //NewtonsoftJson não funciona mais apartir do Core 3.1
+      //  [JsonPropertyName("codigo")]
         public int Id { get; set; }
+
+        [JsonPropertyName("Nome")]
         public string FirstName { get; set; }
+
+        //[JsonPropertyName("Sobrenome")]
         public string LastName { get; set; }
+
+        //[JsonPropertyName("Endereço")]
         public string Address { get; set; }
+
+        //[JsonIgnore]
         public string Gender { get; set; }
     }
 }
